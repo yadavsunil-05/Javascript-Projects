ac94360ad0b800534c86708e3b99af32bce539d7
Sunil <sy992455@gmail.com>
2022-05-02 23:12:30 +0530

    Initial Commit

--- RegistrationFormValidation/index.js
+++ RegistrationFormValidation/index.js
@@ -1,11 +1,68 @@
+let firstNameInvalid = document.getElementById('firstNameInvalid');
+let firstNameValid = document.getElementById('firstNameValid');
+let lastNameInvalid = document.getElementById('lastNameInvalid');
+let lastNameValid = document.getElementById('lastNameValid');
+let emailInvalid = document.getElementById('emailInvalid');
+let emailValid = document.getElementById('emailValid');
+let phoneInvalid = document.getElementById('phoneInvalid');
+let phoneValid = document.getElementById('phoneValid');
+let zipCodeInvalid = document.getElementById('zipCodeInvalid');
+let zipCodeValid = document.getElementById('zipCodeValid');
+
+
 
 let validateForm = function () {
-  console.log("Validetede sucesfully")
+  console.log("Validated sucesfully")
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phoneNumber").value;
   let state = document.getElementById("state").value;
   let zipcode = document.getElementById("zip-code").value;
-  console.log(firstName, lastName, email, phone, state, zipcode);
+
+  if (firstName === "") {
+    firstNameInvalid.style.display = 'block';
+    firstNameValid.style.display = 'none';
+  }
+  else {
+    firstNameValid.style.display = 'block';
+    firstNameInvalid.style.display = 'none';
+  }
+
+  if (lastName === "") {
+    lastNameInvalid.style.display = "block";
+    lastNameValid.style.display = 'none';
+  }
+  else {
+    lastNameValid.style.display = 'block';
+    lastNameInvalid.style.display = "none";
+  }
+
+  if (email === "" || !email.includes('@') || email.startsWith('@') || !email.includes('.') ||
+    email.slice(email.lastIndexOf('.')).length < 3) {  //Error Condition
+    emailInvalid.style.display = 'block';
+    emailValid.style.display = 'none';
+  }
+  else {
+    emailInvalid.style.display = 'none';
+    emailValid.style.display = 'block';
+  }
+
+  if (phone === "" || phone.includes('.') || phone.length != 10 || Number(phone[0]) < 6) {
+    phoneInvalid.style.display = 'block';
+    phoneValid.style.display = 'none';
+  }
+  else {
+    phoneInvalid.style.display = 'none';
+    phoneValid.style.display = 'block';
+  }
+
+  if (zipcode === "" || zipcode.includes('.') || zipcode.length != 6) {
+    zipCodeInvalid.style.display = 'block';
+    zipCodeValid.style.display = 'none';
+  }
+  else {
+    zipCodeInvalid.style.display = 'none';
+    zipCodeValid.style.display = 'block';
+  }
 }
