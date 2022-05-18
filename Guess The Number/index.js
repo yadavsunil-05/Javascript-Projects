055e99d0ac220db87e473e119c88d76630fbf5b7
Sunil <sy992455@gmail.com>
2022-05-18 12:13:40 +0530

    Inital Commit

--- Guess The Number/index.js
+++ Guess The Number/index.js
@@ -1,6 +1,9 @@
 
 let computerGuess;
 let userGuess = [];
+let userUpdate = document.getElementById('textOutput');
+let userInputToEmpty = document.getElementById('inputBox');
+
 
 //Init Method IIFE
 (function () {
@@ -15,11 +18,32 @@ const startGame = () => {
   document.getElementById('welcomeScreen').style.display = "none"
 }
 
+//Logic Behind the App
+const compareGuess = () => {
+  let userNum = Number(document.getElementById('inputBox').value);
+  userGuess.push(userNum)   // userGuess = [...userGuess,userNum]  Spread Operator
+  document.getElementById('guesses').innerText = userGuess.join(' ');
+  //Comparing the User Guess with the target(computerGuess)
+  if (userNum > computerGuess) {
+    userUpdate.innerText = "Your guess is High 😲";
+  }
+  else if (userNum < computerGuess) {
+    userUpdate.innerText = "Your guess is Low 😌";
+  }
+  else {
+    userUpdate.innerText = "It's Correct 😀";
+  }
+  userInputToEmpty.value = "";  //Deleting the text in input box
+}
+
+
 
 const easyMode = () => {
+  let maxGuess = 10;
   startGame();
 }
 
 const hardMode = () => {
+  let maxGuess = 5;
   startGame();
 }
