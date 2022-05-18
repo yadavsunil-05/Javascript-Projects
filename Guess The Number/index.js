e4e2ccde1ab6d882aed7e7e40dd6800ba27683f3
Sunil <sy992455@gmail.com>
2022-05-18 09:24:50 +0530

    Initial Commit

--- Guess The Number/index.js
+++ Guess The Number/index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+
+let computerGuess;
+let userGuess = [];
+
+//Init Method IIFE
+(function () {
+  computerGuess = Math.trunc(Math.random() * 100)
+  document.getElementById('newGameButton').style.display = 'none'
+  document.getElementById('gameArea').style.display = 'none'
+})();
+
