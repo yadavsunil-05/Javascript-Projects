c19300f4bb97cea17d16958495e13cf17d214cf6
Sunil <sy992455@gmail.com>
2022-05-28 10:44:20 +0530

    Initial Commit

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,17 +1,60 @@
-let str = "kamlesshk"
-let obj = {}
-const map = new Map();
+let turnping = new Audio("ting.mp3");
+let turn = "X";
+let gameOver = false;
 
-for (let i = 0; i < str.length; i++) {
-  if (map.has(str[i]))
-    map.set(str[i], map.get(str[i]) + 1)
-  else
-    map.set(str[i], 1)
+//Function to change Turn
+const changeTurn = () => {
+  return turn === "X" ? "0" : "X";
+};
+
+//Function to check win
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
+  console.log(boxtext);
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  wins.forEach((mat) => {
+    if (
+      (boxtext[mat[0]].innerText === boxtext[mat[1]].innerText ) && ( boxtext[mat[1]].innerText === boxtext[mat[2]].innerText) && (boxtext[mat[0]].innerText !== "")
+    ) {
+      document.getElementById("changeText").innerText = ` Hurray!!! ${boxtext[mat[0]].innerText}  Won.🏆`;
+      gameOver = true;
+      document.querySelector(".imgbox").getElementsByTagName('img')[0].style.width ="45%"
+      setTimeout(()=>{
+        resetTicTacToe()
+      },3000)
+    } 
+  });
+};
+
+//Logic behind the tictactoe
+let boxes = document.getElementsByClassName("box");
+//return collection converting in array
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      turnping.play();
+      checkWin();
+      if(!gameOver) document.getElementById("changeText").innerText = `Turn For : ${turn}`;
+    }
+  });
+});
+
+
+
+//Adding Onclick EventListner on reset Button
+const resetTicTacToe = ()=>{
+    window.location.reload()
 }
-
-for (let [char, freq] of map) {
-  obj[char] = freq
-}
-console.log(obj)
-
-
