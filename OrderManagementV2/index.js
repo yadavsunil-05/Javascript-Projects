ceb1cbb92f4ad79a1d79b31a16fd7c863ec46786
Sunil <sy992455@gmail.com>
2022-06-02 10:38:27 +0530

    Initial Commit

--- OrderManagementV2/index.js
+++ OrderManagementV2/index.js
@@ -1,13 +1,63 @@
-const button = document.querySelector(".btn-container button")
-const orderBlock = document.querySelectorAll(".order-block")
-const cardHeader = document.querySelectorAll(".card-header")
-const orderIdInput = document.querySelector(".order-id-input")
+const button = document.querySelector(".btn-container button");
+const parentDiv = document.querySelector("#main-container-row");
+let index = 0
+//Append Prepend Before After
+button.addEventListener("click", () => {
+  let orderIdInput = document.querySelector(".order-id-input");
+  createNewOrderColumn(orderIdInput);
+});
 
-console.log(orderBlock)
-console.log(cardHeader)
-let cnt = 0;
-button.addEventListener('click', () => {
-  cardHeader[cnt].textContent = ` Order Id:${orderIdInput.value}`
-  orderBlock[cnt].style.display = "block"
-  cnt++;
-})
+
+const createNewOrderColumn = (orderIdInput) => {
+
+  let colDiv = document.createElement("div");
+  let cardDiv = document.createElement("div");
+
+  let cardHeaderDiv = document.createElement("div");
+  // cardHeaderDiv.textContent = "Order Id:";
+
+
+  let cardBodyDiv = document.createElement("div");
+  let h5 = document.createElement("h5");
+  h5.textContent = "Bill Amount:";
+  let p = document.createElement("p");
+  p.textContent = "Items:Pizza,Coke";
+  let button = document.createElement("a");
+  button.textContent = "Cancel Order";
+  cardBodyDiv.append(h5);
+  cardBodyDiv.append(p);
+  cardBodyDiv.append(button);
+
+  let cardFooter = document.createElement("div");
+  cardFooter.textContent = "Pizza Baked";
+
+  cardDiv.append(cardHeaderDiv);
+  cardDiv.append(cardBodyDiv);
+  cardDiv.append(cardFooter);
+  colDiv.append(cardDiv);
+  parentDiv.append(colDiv)
+
+  //Adding Class To newlly created Nodes
+  colDiv.setAttribute("class", "col-md-3 col-xs-12 order-block")
+  cardDiv.setAttribute("class", "card text-center")
+  cardHeaderDiv.setAttribute("class", "card-header")
+  cardBodyDiv.setAttribute("class", "card-body")
+  h5.setAttribute('class', "card-title")
+  p.setAttribute("class", "card-text")
+  button.setAttribute("class", "btn btn-danger")
+  cardFooter.setAttribute("class", "card-footer text-muted")
+
+  //Displaying the order id 
+  document.querySelectorAll('.card-header')[index++].textContent = `Order Id : ${orderIdInput.value}`
+
+  //Cancel button event handler
+  document.querySelectorAll('.btn-danger').forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      console.log(event.target.parentNode.parentNode.parentNode.remove())
+    })
+  })
+}
+
+
+
+
