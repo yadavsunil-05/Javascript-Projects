46050925c9aec03f72fa7ffd82901d44036059c3
Sunil <sy992455@gmail.com>
2022-06-10 12:44:13 +0530

    Initial Commit

--- ShoppingApp-V2/Js/createCartItem.js
+++ ShoppingApp-V2/Js/createCartItem.js
@@ -13,8 +13,52 @@ cartModal.addEventListener('click', () => {
 }, true)
 
 const createCartItem = (cartProduct) => {
-  const cartContainer = `
-<div id="cartSingleItem">
+
+  const cartSingleDiv = document.createElement('div');
+  const cartProdImgDiv = document.createElement('div');
+  const cartProdImgTag = document.createElement('img');
+  const cartProdBodyDiv = document.createElement('div');
+  const cartProdHeaderDiv = document.createElement('div');
+  const cartProdTitleH5 = document.createElement('h5')
+  const cartProdPriceH4 = document.createElement('h4')
+  const cartProdRemoveDiv = document.createElement('div');
+  const cartRemoveBtn = document.createElement('button');
+
+
+  parentCartBox.append(cartSingleDiv)
+  cartSingleDiv.append(cartProdImgDiv)
+  cartProdImgDiv.append(cartProdImgTag)
+  cartSingleDiv.append(cartProdBodyDiv)
+  cartProdBodyDiv.append(cartProdHeaderDiv)
+  cartProdHeaderDiv.append(cartProdTitleH5)
+  cartProdHeaderDiv.append(cartProdPriceH4)
+  cartProdBodyDiv.append(cartProdRemoveDiv)
+  cartProdRemoveDiv.append(cartRemoveBtn)
+  cartRemoveBtn.textContent = "Remove"
+
+  cartSingleDiv.setAttribute('id', "cartSingleItem")
+  cartProdImgDiv.setAttribute("class", "cartProductImg")
+  cartProdImgTag.setAttribute("class", "cartImg")
+  cartProdBodyDiv.setAttribute("class", "cartProductBody")
+  cartProdHeaderDiv.setAttribute("class", "cartProductHeader")
+  cartProdTitleH5.setAttribute("class", "cartProductTitle")
+  cartProdPriceH4.setAttribute("class", "card-title cartProductPrice")
+  cartProdRemoveDiv.setAttribute("class", "cartProductRemove")
+  cartRemoveBtn.setAttribute("class", "cartRemoveBtn")
+
+  cartProdImgTag.setAttribute('src', cartProduct.image);
+  cartProdImgTag.setAttribute('width', "75px");
+  cartProdTitleH5.textContent = `${cartProduct.title.slice(0, 10)}...`
+  cartProdPriceH4.textContent = `$${cartProduct.price}`
+
+
+  cartRemoveBtn.addEventListener('click', function () {
+    cartSingleDiv.style.display = "none"
+  })
+}
+
+
+`<div id="cartSingleItem">
   <div class="cartProductImg">
     <img width="75px" class="cartImg">
   </div>
@@ -29,14 +73,4 @@ const createCartItem = (cartProduct) => {
   </div>
 </div>
 `
-  parentCartBox.insertAdjacentHTML("beforeend", cartContainer)
-
-  const cartProdImg = document.querySelector('.cartImg')
-  const cartProductTitle = document.querySelector('.cartProductTitle')
-  const cartProductPrice = document.querySelector('.cartProductPrice')
-
-  cartProdImg.setAttribute('src', cartProduct.image);
-  cartProductTitle.textContent = `${cartProduct.title.slice(0, 10)}...`
-  cartProductPrice.textContent = `$${cartProduct.price}`
-}
 
